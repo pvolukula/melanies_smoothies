d314d70bd694c5ca542f3b57e5b402d162ae087a
pvolukula <prathap.volukula@gmail.com>
2024-08-13 03:29:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
 # Write directly to the app
 st.title(":apple: Customize your Smoothie :apple:")
